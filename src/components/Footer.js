783233188cc5892e7315712b7795ff01c0ec37bb
Saurav Ghimire <148796724+SauravGhm@users.noreply.github.com>
2024-04-17 10:13:32 -0500

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-    <footer className="footer">
-      © 2024 MyApp. All rights reserved.
-    </footer>
-  );
-  export default Footer;
-  
--- src/components/ProgressBar.js
+++
@@ -1,6 +0,0 @@
-const ProgressBar = ({ progress }) => (
-    <div className="progress-bar">
-      <div className="progress-bar-fill" style={{ width: `${progress}%` }}></div>
-    </div>
-  );
-  export default ProgressBar;
