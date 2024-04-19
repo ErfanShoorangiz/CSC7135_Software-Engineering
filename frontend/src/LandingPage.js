5df2f99e92b93452a1d75e7c31f5f12ae8c16be9
Saurav Ghimire <sauravghimire@Sauravs-MacBook-Air.local>
2024-04-19 14:08:55 -0500

    front end commit

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './LandingPage.css'; //This is our custom CSS file
+
+class LandingPage extends React.Component{
+    state = {tweet: ''};
+}
+handleInputChange = (event)=>{
+    this.setState({tweet: event.target.value});
+};
+handleSubmit = (event)=>{
+    event.preventDefault();
+    fetch('http://localhost:5000/analyze', { // we are currently assuming that the python backend is running on localhost: 5000
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({tweet: this.state.tweet})
+    })
+    .then(response=>response.JSON())
+    .then(data=> alert('Sentiment: $(data.sentiment)')) //displaying sentiment in an alert for simplicity
+    .catch(error=> console.error('Error:', error));
+};
+render(){
+    return(
+        <div className="container">
+        <h1>Welcome to Tweet Sentiment Analyzer</h1>
+        <form onSubmit={this.handleSubmit}>
+            <input
+                type="text"
+                value={this.state.tweet}
+                onChange={this.handleInputChange}
+                placeholder="Enter your tweet here..."
+                className="tweet-input"
+            />
+            <button type="submit" className="submit-button">Analyze Sentiment</button>
+        </form>
+    </div>
+     );
+}
+export default LandingPage;
